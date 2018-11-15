5625136091e3d533fa4a92d809c0162e0bdf5c26
rgjgfde <45072244+rgjgfde@users.noreply.github.com>
2018-11-15 16:56:30 +0200

    Add files via upload

---
+++ main.cpp tudor.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int n;
+int main()
+{ cout<<"N=";cin>>n;
+  int divizor=2;
+  int putere;
+  while (n>1)
+  {
+      putere=0
+      while (divizor=0) {
+            n=n/divizor;
+            putere=putere+1
+
+  }
+
+
+ int suma=1;
+  for (divizor=2; divizor<=n/2; divizor++)
+      if(n%divizor==0)
+      {
+          suma=suma+divizor
+      }
+      if (n==s)
+ {
+     cout<<"Numarul este perfect!"
+ }
+  }
+    }
